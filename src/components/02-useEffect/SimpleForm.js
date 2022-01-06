e0b60047abb7ad53d9fd1bdfb6bf4283990daba4
Administrator <admin@example.com>
2022-01-06 19:47:12 +0100

    Agrega useEffect en SimpleForm

---
+++ src/components/02-useEffect/SimpleForm.js
@@ -0,0 +1,63 @@
+import React, { useEffect, useState } from 'react';
+import './effects.css';
+
+
+export const SimpleForm = () => {
+
+    const [formState, setFormState] = useState({
+        name: '',
+        email: '',
+    });
+
+    const { name, email } = formState;
+
+    useEffect(() => {
+        console.log('useEffect');
+    }, []);
+
+    useEffect(() => {
+        console.log('formState changed');
+    }, [formState]);
+
+    useEffect(() => {
+        console.log('email changed');
+    }, [email]);
+
+    const handleInputChange = ({target}) => {
+        setFormState({
+            ...formState,
+            [target.name]: target.value,
+        });
+    };
+
+    return (
+        <>
+            <h1>Simple Form</h1>
+            <hr/>
+
+            <div className="form-group">
+                <input
+                    type="text"
+                    name="name"
+                    className="form-control"
+                    placeholder="Type your name"
+                    autoComplete="off"
+                    value={name}
+                    onChange={handleInputChange}
+                />
+            </div>    
+
+            <div className="form-group">
+                <input
+                    type="text"
+                    name="email"
+                    className="form-control"
+                    placeholder="name@email.com"
+                    autoComplete="off"
+                    value={email}
+                    onChange={handleInputChange}
+                />
+            </div>              
+        </>
+    )
+}
