a6ff9c9d1576c1f56432320e6190179a2b01fdf0
Administrator <admin@example.com>
2022-01-07 13:33:04 +0100

    Manipula useEffect con prevencion de fuga de memoria

--- src/components/02-useEffect/Message.js
+++ src/components/02-useEffect/Message.js
@@ -1,17 +1,31 @@
-import React, {useEffect} from 'react'
+import React, {useEffect, useState} from 'react'
 
 export const Message = () => {
 
+    const [coords, setCoords] = useState({x: 0, y: 0});
+    const {x, y} = coords
+
     useEffect(() => {
-        console.log('useEffect');
+
+        const mouseMove = (e) => {
+            const coords = {x: e.x, y: e.y};
+            console.log(coords);
+        }
+
+        window.addEventListener('mousemove', mouseMove);
+
         return () => {
-            console.log('cleanup');
+            window.removeEventListener('mousemove', mouseMove);
         }
     }, []);
 
     return (
         <div>
             <h3>Eres genial</h3>
+            <p>
+            <strong>X:</strong> {x}
+            <strong>Y:</strong> {y}
+            </p>
         </div>
     )
 }
