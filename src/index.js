eb03d5b0fab3bf8f1fad879412e7d1c01b5bc6b6
Administrator <admin@example.com>
2022-01-07 14:23:48 +0100

    Agrega Formulario con custom Hook

--- src/index.js
+++ src/index.js
@@ -2,11 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 // import { CounterApp } from './components/01-useState/CounterApp';
 // import { CounterWithCustomHook } from './components/01-useState/CounterWithCustomHook';
-import { SimpleForm } from './components/02-useEffect/SimpleForm';
+// import { SimpleForm } from './components/02-useEffect/SimpleForm';
 // import { HookApp } from './HookApp';
-
+import {FormWithCustomHook} from './components/02-useEffect/FormWithCustomHook';
 
 ReactDOM.render(
-    <SimpleForm />, 
+    <FormWithCustomHook />, 
     document.getElementById('root')
 );
