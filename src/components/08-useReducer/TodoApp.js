d4991c5f61d746e641c89627928fa15cc7c8ebca
Administrator <admin@example.com>
2022-01-11 13:42:48 +0100

    Crea estructura inicial de lista de ToDos

--- src/components/08-useReducer/TodoApp.js
+++ src/components/08-useReducer/TodoApp.js
@@ -17,14 +17,38 @@ export const TodoApp = () => {
 
     return (
         <div>
-            <h1>TodoApp</h1>
+            <h1>TodoApp ({todos.length})</h1>
             <hr/>
+            <div className="row">
+                <div className="col-7">
+                    <ul className="list-group list-group-flush">
+                        {
+                            todos.map((todo, i) => (
+                                <li key={todo.id} className="list-group-item">
+                                    <p className="text-center">{i+1}. {todo.desc}</p>
+                                    <button className="btn btn-danger">Borrar</button>
+                                </li>
+                        ))}
+                    </ul>
+                </div>
+                <div className="col-5">
+                    <h4>Agregar ToDo</h4>
+                    <hr/>
 
-            <ul>
-                <li>Hola</li>
-                <li>Mundo</li>
-                <li>Hola de nuevo</li>
-            </ul>
+                    <form className="d-grid gap-2">
+                        <input
+                            type="text" 
+                            name="desc" 
+                            placeholder="Aprender..." 
+                            autocomplete="off"
+                            className="form-control"
+                        />
+                        <button className="btn btn-outline-primary mt-1">
+                            Agregar
+                        </button>
+                    </form>
+                </div>
+            </div>
         </div>
     )
 }
