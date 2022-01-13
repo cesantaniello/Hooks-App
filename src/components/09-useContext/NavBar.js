f1ef66b8e3f7a7a94d3f9babf14f7163d00e4539
Administrator <admin@example.com>
2022-01-13 12:52:39 +0100

    Agrega Link y NavLink

---
+++ src/components/09-useContext/NavBar.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import {Link, NavLink} from 'react-router-dom';
+
+export const NavBar = () => {
+    return (
+        <nav className="navbar navbar-expand-sm navbar-dark bg-dark">
+            <div className="container-fluid">
+                <Link to="/"className="navbar-brand">NavBar</Link>
+                <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
+                    <div className="navbar-nav">
+                        <NavLink to="./" className="nav-link" aria-current="page">Home</NavLink>
+                        <NavLink to="./about" className="nav-link">About</NavLink>
+                        <NavLink to="./login" className="nav-link">Login</NavLink>
+                    </div>
+                </div>
+            </div>
+        </nav>
+    )
+}
