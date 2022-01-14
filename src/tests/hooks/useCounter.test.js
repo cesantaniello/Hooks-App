95c6e1af4f0a73a9d65f11c4ce7097c0309e1f71
Administrator <admin@example.com>
2022-01-14 13:19:13 +0100

    Ejecuta funciones del custom Hook en las pruebas

--- src/tests/hooks/useCounter.test.js
+++ src/tests/hooks/useCounter.test.js
@@ -1,15 +1,45 @@
-import {renderHook} from '@testing-library/react-hooks';
+import {renderHook, act} from '@testing-library/react-hooks';
 import {useCounter} from '../../hooks/useCounter';
 
 describe('Tests on useCounter', () => {
     test('useCounter should return default values', () => {
         const { result } = renderHook(() => useCounter());
 
-        console.log(result.current);
-
         expect(result.current.counter).toBe(10);
         expect(result.current.increment).toBeInstanceOf(Function);
         expect(result.current.decrement).toBeInstanceOf(Function);
         expect(result.current.reset).toBeInstanceOf(Function);
     });
+
+    test('useCounter should return 100', () => {
+        const { result } = renderHook(() => useCounter(100));
+        expect(result.current.counter).toBe(100);
+    });
+
+    test('should increment counter on 1', () => {
+        const { result } = renderHook(() => useCounter(100));
+        act(() => {
+            result.current.increment();
+        });
+        expect(result.current.counter).toBe(101);
+    })
+
+    test('should decrement counter on 1', () => {
+        const { result } = renderHook(() => useCounter(101));
+        act(() => {
+            result.current.decrement();
+        });
+        expect(result.current.counter).toBe(100);
+    })
+
+    test('should reset counter to initialState', () => {
+        const { result } = renderHook(() => useCounter(100));
+        
+        act(() => {
+            result.current.decrement();      
+            result.current.reset();
+        });
+
+        expect(result.current.counter).toBe(100);
+    })
 })
